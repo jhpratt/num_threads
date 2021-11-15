e8c10379a03993e47d24388ca339291df4a5168c
Thomas Hurst <tom@hur.st>
2021-11-15 18:26:37 +0000

    Fix memory leak on FreeBSD (#2)

--- src/freebsd.rs
+++ src/freebsd.rs
@@ -9,5 +9,10 @@ extern "C" {
 pub(crate) fn num_threads() -> Option<NonZeroUsize> {
     // Safety: `kinfo_getproc` and `getpid` are both thread-safe. All invariants of `as_ref` are
     // upheld.
-    NonZeroUsize::new(unsafe { kinfo_getproc(libc::getpid()).as_ref() }?.ki_numthreads as usize)
+    unsafe {
+        let kip = kinfo_getproc(libc::getpid());
+        let num_threads = NonZeroUsize::new(kip.as_ref()?.ki_numthreads as usize);
+        libc::free(kip as *mut libc::c_void);
+        num_threads
+    }
 }
