4783e88596e8c4cc05561443a838ba9b8af4ebb6
Thomas Hurst <tom@hur.st>
2021-11-16 01:58:25 +0000

    Linux: Fix errors on earlier Rust versions

--- src/linux.rs
+++ src/linux.rs
@@ -1,12 +1,14 @@
+use std::fs;
 use std::num::NonZeroUsize;
 
 pub(crate) fn num_threads() -> Option<NonZeroUsize> {
-    std::fs::read_to_string("/proc/self/stat")
+    fs::read_to_string("/proc/self/stat")
         .ok()
         .as_ref()
         // Skip past the pid and (process name) fields
         .and_then(|stat| stat.rsplit(')').next())
         // 20th field, less the two we skipped
         .and_then(|rstat| rstat.split_whitespace().nth(17))
-        .and_then(|num_threads| num_threads.parse().ok())
+        .and_then(|num_threads| num_threads.parse::<usize>().ok())
+        .and_then(NonZeroUsize::new)
 }
