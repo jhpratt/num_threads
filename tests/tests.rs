66a1be9b1cc1b2decdda18e399e9eae09c17530f
Thomas Hurst <tom@hur.st>
2023-01-31 23:15:21 +0000

    Rewrite tests
    Previously a macro was used to recursively spawn threads to perform
    asserts within - meaning failures involved confusing and ugly chains of
    thread join panics.
    
    Replace this with a function that spins up a given number of threads and
    calls a closure on the main thread.  This leads to much tidier failure
    output.
    
    Add some feedback for which test is running to aid in debugging
    failures.

--- tests/tests.rs
+++ tests/tests.rs
@@ -1,47 +1,86 @@
-
 extern crate num_threads;
 
+use std::io::prelude::*;
 use std::num::NonZeroUsize;
 
-// Run each expression in its own thread.
-macro_rules! threaded {
-    ($first:expr;) => {
-        $first;
-    };
-    ($first:expr; $($rest:expr;)*) => {
-        $first;
-        ::std::thread::spawn(|| {
-            threaded!($($rest;)*);
-        })
-        .join()
-        .unwrap();
-    };
+/// Run a closure with n threads running in the background
+fn with_threads<F>(n: usize, f: F)
+where
+    F: FnOnce(),
+{
+    let mut threads = vec![];
+    let lock = ::std::sync::Arc::new(::std::sync::Mutex::new(()));
+    let _guard = lock.lock().unwrap();
+
+    for _ in 1..n {
+        let lock = lock.clone();
+        threads.push(::std::thread::spawn(move || {
+            drop(lock.lock().unwrap());
+        }));
+    }
+
+    f();
+
+    drop(_guard);
+
+    for handle in threads {
+        handle.join().unwrap();
+    }
 }
 
 fn test_num_threads() {
-    threaded! {
-        assert_eq!(num_threads::num_threads().map(NonZeroUsize::get), Some(1));
-        assert_eq!(num_threads::num_threads().map(NonZeroUsize::get), Some(2));
-        assert_eq!(num_threads::num_threads().map(NonZeroUsize::get), Some(3));
-        assert_eq!(num_threads::num_threads().map(NonZeroUsize::get), Some(4));
-        assert_eq!(num_threads::num_threads().map(NonZeroUsize::get), Some(5));
-        assert_eq!(num_threads::num_threads().map(NonZeroUsize::get), Some(6));
-    }
+    print!("test num_threads: ");
+    ::std::io::stdout().flush().unwrap();
+
+    with_threads(1, || {
+        assert_eq!(num_threads::num_threads().map(NonZeroUsize::get), Some(1))
+    });
+    with_threads(2, || {
+        assert_eq!(num_threads::num_threads().map(NonZeroUsize::get), Some(2))
+    });
+    with_threads(3, || {
+        assert_eq!(num_threads::num_threads().map(NonZeroUsize::get), Some(3))
+    });
+    with_threads(4, || {
+        assert_eq!(num_threads::num_threads().map(NonZeroUsize::get), Some(4))
+    });
+    with_threads(5, || {
+        assert_eq!(num_threads::num_threads().map(NonZeroUsize::get), Some(5))
+    });
+    with_threads(6, || {
+        assert_eq!(num_threads::num_threads().map(NonZeroUsize::get), Some(6))
+    });
+
+    println!("ok");
 }
 
 fn test_is_single_threaded() {
-    threaded! {
-        assert_eq!(num_threads::is_single_threaded(), Some(true));
-        assert_eq!(num_threads::is_single_threaded(), Some(false));
-        assert_eq!(num_threads::is_single_threaded(), Some(false));
-        assert_eq!(num_threads::is_single_threaded(), Some(false));
-        assert_eq!(num_threads::is_single_threaded(), Some(false));
-        assert_eq!(num_threads::is_single_threaded(), Some(false));
-    }
+    print!("test is_single_threaded: ");
+    ::std::io::stdout().flush().unwrap();
+
+    with_threads(1, || {
+        assert_eq!(num_threads::is_single_threaded(), Some(true))
+    });
+    with_threads(2, || {
+        assert_eq!(num_threads::is_single_threaded(), Some(false))
+    });
+    with_threads(3, || {
+        assert_eq!(num_threads::is_single_threaded(), Some(false))
+    });
+    with_threads(4, || {
+        assert_eq!(num_threads::is_single_threaded(), Some(false))
+    });
+    with_threads(5, || {
+        assert_eq!(num_threads::is_single_threaded(), Some(false))
+    });
+    with_threads(6, || {
+        assert_eq!(num_threads::is_single_threaded(), Some(false))
+    });
+
+    println!("ok");
 }
 
 fn main() {
     test_is_single_threaded();
     test_num_threads();
 }
-
